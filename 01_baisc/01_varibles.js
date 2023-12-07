2d8602c2e8848f8171225464d82f02a5dcfd9175
Sourabh dhiman <139675066+sourabhdhiman1262@users.noreply.github.com>
2023-12-07 13:46:39 +0000

    varibles study

---
+++ 01_baisc/01_varibles.js
@@ -0,0 +1,19 @@
+const accountId= 1352;
+let accountName = " Sourabh Dhiman ";
+var accountPassword = 76464 ; 
+accountCity = " Saharanpur " ;
+let AccountState;
+var AccountCountry = null;
+
+// accountId = 23 // this is not allowed
+
+console.log(accountCity)
+
+accountName = " Sam "
+accountPassword = " 46476476 "
+accountCity =" Noida "
+
+console.table([accountId,accountName,accountPassword,accountCity,AccountState,AccountCountry]);
+
+//  don't use var
+// because of issue in block scope and functional scope
