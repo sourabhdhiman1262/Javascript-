26792ce3215875419ae520bfebb542e26780f5ae
Sourabh dhiman <139675066+sourabhdhiman1262@users.noreply.github.com>
2023-12-07 17:17:46 +0000

    datatype study

---
+++ 01_baisc/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict";
+// treat as a newer version of javasript 
+
+let Name = "sourabh";
+let age = 21;
+let logIn = true;
+
+// number = 2 to power 53(range)
+// bigint
+// string = ""
+// boolean = true/ false
+// null = standalone value
+// undefined = 
+// symbol = unique 
+
+// Object 
+
+console.log(typeof undefined) //undefined
+
+console.log(typeof null) //object (this is javascipt language bug)
