d3524e225b07015d463b96b2b07e0cb01d2ebd0f
Sourabh dhiman <139675066+sourabhdhiman1262@users.noreply.github.com>
2023-12-08 15:24:07 +0000

    conversion study

---
+++ 01_baisc/03_coversion.js
@@ -0,0 +1,24 @@
+let score = 45
+let plyerName = "Sourabh" 
+
+// console.log(score);
+// console.log(typeof score);
+
+const NumberScore = Number(plyerName)
+// console.log(typeof NumberScore);
+// console.log(NumberScore);
+
+let seatnumber = String(score)
+// console.log(typeof seatnumber)
+
+let PlayerOnline = Boolean(plyerName)
+console.log(typeof PlayerOnline);
+console.log(PlayerOnline);
+
+/* conversion 
+
+to convet value to string => String()
+to convet value to number => Number()
+to convet value to boolean => Boolean()
+
+*/
