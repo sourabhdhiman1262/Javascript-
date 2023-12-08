11a957437170a3b7a4886c18946184ad92926fbc
Sourabh dhiman <139675066+sourabhdhiman1262@users.noreply.github.com>
2023-12-08 16:39:21 +0000

    comparison study

---
+++ 01_baisc/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(4 > 3);
+// console.log(4 < 3);
+// console.log(4 >= 3);
+// console.log(4 != 3);
+// console.log(4 == 3);
+
+// console.log("2" > 1 );
+// console.log("02" > 1 );
+
+// in upper case javasript auto convert values datatype 
+ 
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);  // ans is ture because the reason is that an equality check == and comparison ><= , <=/>= work diffently.
+/*
+ comparison convert null to a number , treating it as 0.
+that why (3) null >= 0 is ture and other are null>0 is false
+*/
+
+// ===
+console.log("4" == 4);
+//  in this === javasript check is data tpye also comparitve to == .
