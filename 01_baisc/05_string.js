84247cb6559b5b0617c3ccf60016fd5c474783cf
Sourabh dhiman <139675066+sourabhdhiman1262@users.noreply.github.com>
2024-07-29 07:06:20 +0000

    string study

---
+++ 01_baisc/05_string.js
@@ -0,0 +1,16 @@
+let Name = new String("Sourabh ");
+let repoCount = 15;
+
+// ---------- old way to add string    ----------
+
+// console.log(Name + repoCount);
+
+// this syntex is old don't use that insted use moden syntex like,
+
+// ---------- Mordon way ---------
+
+// console.log(`My Name is ${} and my repocount is ${}`);
+
+// in which we use backticks `` using backticks is called string interpoltion in which we creating place holders ${}  so that we can add simple or even perform any operation like charactart, indexof ex. 
+
+console.log(`My Name is ${Name} and my repocount is ${repoCount}`);
